1a4b3808ac40a1f3c1f84422881483e295487231
jamie <neworderofjamie@gmail.com>
2018-02-26 18:55:00 +0000

    Various NN fun * Use 'Xavier' initialisation to magically improve learning time * Use in-silica rotations to generate more training data (disabled) * Catch keyboard exceptions so you can stop training

--- train_autoencoder.py
+++ train_autoencoder.py
@@ -2,44 +2,61 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
-from cv2 import imread, IMREAD_GRAYSCALE
+from cv2 import imread, resize, IMREAD_GRAYSCALE
 from glob import glob
 
 #-----------------------------------------------------------------------------
 # Parameters
 #-----------------------------------------------------------------------------
-SNAPSHOT_WIDTH = 180
-SNAPSHOT_HEIGHT = 50
+SNAPSHOT_WIDTH = 90#69
+SNAPSHOT_HEIGHT = 25
 
 # Calculate layer sizes
-LAYER_SIZES = [SNAPSHOT_WIDTH * SNAPSHOT_HEIGHT, 2048, 512, 128, 64]
+#LAYER_SIZES = [SNAPSHOT_WIDTH * SNAPSHOT_HEIGHT, 3072, 1024, 256, 64]
+LAYER_SIZES = [SNAPSHOT_WIDTH * SNAPSHOT_HEIGHT, 1024, 512, 128, 64]
 
 # Training Parameters
 LEARNING_RATE = 0.001
 BATCH_SIZE = 100
-DISPLAY_STEP = 1000
+DISPLAY_STEP = 250
 
 NUM_STEPS = 10000
 
+# How many training images should we keep behind for testing
+# **NOTE** this is kinda unnecessary as the
 NUM_TESTING = 10
 
+# How many in-silica 'rolls' should we make of each training image - this generates rotated versions
+NUM_IMAGE_ROLLS = 1
+IMAGE_ROLL_PIXELS = SNAPSHOT_WIDTH / NUM_IMAGE_ROLLS
+
+# Should we train the network or just load the exported model
 TRAIN = True
 
 #-----------------------------------------------------------------------------
 # Entry point
 #-----------------------------------------------------------------------------
 # Search for route data
-training_images = glob("office_training_2/*.png")
+training_images = glob("office_training_3/*.png")
 
 # Create empty array of training data
-data = np.empty(dtype=np.float32, shape=(len(training_images), LAYER_SIZES[0]))
+data = np.empty(dtype=np.float32, shape=(len(training_images) * NUM_IMAGE_ROLLS, LAYER_SIZES[0]))
 
 # Load route images into rows
 for i, t in enumerate(training_images):
     # Read image
     image = imread(t, IMREAD_GRAYSCALE).astype(np.float32)
+    #image = image[:,15:84]
     image /= 255.0
-    data[i] = np.reshape(image, LAYER_SIZES[0])
+
+    # Copy into first row corresponding to this image
+    data[i * NUM_IMAGE_ROLLS] = np.reshape(image, LAYER_SIZES[0])
+
+    # Loop through in-silica 'rolls'
+    for r in range(1, NUM_IMAGE_ROLLS):
+        # Roll image suitable amount
+        rolled_image = np.roll(image, r * IMAGE_ROLL_PIXELS, axis=1)
+        data[(i * NUM_IMAGE_ROLLS) + r] = np.reshape(rolled_image, LAYER_SIZES[0])
 
 # Shuffle dataset
 # **NOTE** this only shuffles along 1st axis as required
@@ -56,14 +73,16 @@ with tf.Session() as sess:
         # tf Graph input (only pictures)
         X = tf.placeholder("float", [None, LAYER_SIZES[0]], name="input")
 
-        encoder_weights = [tf.Variable(tf.random_normal([layer_size, next_layer_size]))
+        # **NOTE** 'Xavier' initialization speeds up learning immensely
+        initializer = tf.contrib.layers.xavier_initializer()
+        encoder_weights = [tf.Variable(initializer([layer_size, next_layer_size]))
                            for layer_size, next_layer_size in zip(LAYER_SIZES[:-1], LAYER_SIZES[1:])]
-        decoder_weights = [tf.Variable(tf.random_normal([layer_size, next_layer_size]))
+        decoder_weights = [tf.Variable(initializer([layer_size, next_layer_size]))
                            for layer_size, next_layer_size in reversed(zip(LAYER_SIZES[1:], LAYER_SIZES[:-1]))]
 
-        encoder_biases = [tf.Variable(tf.random_normal([layer_size]))
+        encoder_biases = [tf.Variable(initializer([layer_size]))
                           for layer_size in LAYER_SIZES[1:]]
-        decoder_biases = [tf.Variable(tf.random_normal([layer_size]))
+        decoder_biases = [tf.Variable(initializer([layer_size]))
                           for layer_size in reversed(LAYER_SIZES[:-1])]
 
         # Building the encoder
@@ -104,41 +123,43 @@ with tf.Session() as sess:
         # Run the initializer
         sess.run(init)
 
-        print("Training:")
+        print("Training (ctrl-c to stop)")
+        try:
+            # Training
+            index_in_epoch = 0
+            for i in range(NUM_STEPS):
+                start = index_in_epoch
 
-        # Training
-        index_in_epoch = 0
-        for i in range(NUM_STEPS):
-            start = index_in_epoch
+                # Go to the next epoch
+                batch_x = None
+                if (index_in_epoch + BATCH_SIZE) > training_data.shape[0]:
+                    # Get the rest examples in this epoch
+                    rest_num_examples = training_data.shape[0] - index_in_epoch
+                    images_rest_part = training_data[start:training_data.shape[0]]
 
-            # Go to the next epoch
-            batch_x = None
-            if (index_in_epoch + BATCH_SIZE) > training_data.shape[0]:
-                # Get the rest examples in this epoch
-                rest_num_examples = training_data.shape[0] - index_in_epoch
-                images_rest_part = training_data[start:training_data.shape[0]]
+                    # Shuffle dataset
+                    # **NOTE** this only shuffles along 1st axis as required
+                    np.random.shuffle(training_data)
 
-                # Shuffle dataset
-                # **NOTE** this only shuffles along 1st axis as required
-                np.random.shuffle(training_data)
+                    # Start next epoch
+                    start = 0
+                    index_in_epoch = BATCH_SIZE - rest_num_examples
+                    end = index_in_epoch
+                    images_new_part = training_data[start:end]
 
-                # Start next epoch
-                start = 0
-                index_in_epoch = BATCH_SIZE - rest_num_examples
-                end = index_in_epoch
-                images_new_part = training_data[start:end]
+                    batch_x = np.concatenate((images_rest_part, images_new_part), axis=0)
+                else:
+                    index_in_epoch += BATCH_SIZE
+                    end = index_in_epoch
+                    batch_x = training_data[start:end]
 
-                batch_x = np.concatenate((images_rest_part, images_new_part), axis=0)
-            else:
-                index_in_epoch += BATCH_SIZE
-                end = index_in_epoch
-                batch_x = training_data[start:end]
+                # Run optimization op (backprop) and cost op (to get loss value)
+                _, l = sess.run([optimizer, loss], feed_dict={X: batch_x})
 
-            # Run optimization op (backprop) and cost op (to get loss value)
-            _, l = sess.run([optimizer, loss], feed_dict={X: batch_x})
-
-            if i == 0 or i % DISPLAY_STEP == 0:
-                print("Step %u, Minibatch loss:%f" % (i, l))
+                if i == 0 or i % DISPLAY_STEP == 0:
+                    print("Step %u, Minibatch loss:%f" % (i, l))
+        except KeyboardInterrupt:
+            pass
 
         # Saving
         builder = tf.saved_model.builder.SavedModelBuilder("./export")
