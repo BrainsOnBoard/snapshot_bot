a2facb9485ec9efd9a171f2620916b7302e75d5d
Alex Dewar <a.dewar@sussex.ac.uk>
2019-07-03 14:35:11 +0100

    Delete unneeded build.sh

--- build.sh
+++
@@ -1,2 +0,0 @@
-g++ snapshot_bot.cc -std=c++11 -Wall -Wpedantic -pthread `pkg-config --libs --cflags opencv` -I $GENN_ROBOTICS_PATH -o snapshot_bot
-g++ computer.cc -std=c++11 -Wall -Wpedantic -pthread `pkg-config --libs --cflags opencv` -I $GENN_ROBOTICS_PATH -o computer
